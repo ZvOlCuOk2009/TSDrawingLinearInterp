819d63d8e60e4510890418c2301b5ab2e30f6609
Zvigun Alexandr <mircamnia@ukr.net>
2015-11-30 11:32:27 +0200

    one commit

---
+++ TestDrawingLinearInterp/TSLinearInterp.h
@@ -0,0 +1,13 @@
+//
+//  TSLinearInterp.h
+//  TestDrawingLinearInterp
+//
+//  Created by Mac on 30.11.15.
+//  Copyright © 2015 Tsvigun Alexandr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TSLinearInterp : UIView
+
+@end
---
+++ TestDrawingLinearInterp/TSLinearInterp.m
@@ -0,0 +1,21 @@
+//
+//  TSLinearInterp.m
+//  TestDrawingLinearInterp
+//
+//  Created by Mac on 30.11.15.
+//  Copyright © 2015 Tsvigun Alexandr. All rights reserved.
+//
+
+#import "TSLinearInterp.h"
+
+@implementation TSLinearInterp
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
