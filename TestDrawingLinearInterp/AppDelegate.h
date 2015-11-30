ab62dc4519d7a01046f4a6fb9ebd773e65896493
Zvigun Alexandr <mircamnia@ukr.net>
2015-11-30 09:52:50 +0200

    Initial Commit

---
+++ TestDrawingLinearInterp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestDrawingLinearInterp
+//
+//  Created by Mac on 30.11.15.
+//  Copyright © 2015 Tsvigun Alexandr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestDrawingLinearInterp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestDrawingLinearInterp
+//
+//  Created by Mac on 30.11.15.
+//  Copyright © 2015 Tsvigun Alexandr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
